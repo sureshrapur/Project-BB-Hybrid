06a3914130c851a3ff02615f98c8507f3af7b474
Suresh Rapur <suresh@sjsoftech.com>
2015-05-04 10:19:30 +0000

    Changes from Screen Builder

--- Project BB Hybrid/activationView/index.js
+++ Project BB Hybrid/activationView/index.js
@@ -3,3 +3,13 @@
 app.activationView = kendo.observable({
     onShow: function() {}
 });
+(function(parent) {
+    var activationViewModel = kendo.observable({
+        fields: {
+            activationCode: '',
+        },
+        submit: function() {}
+    });
+
+    parent.set('activationViewModel', activationViewModel);
+})(app.activationView);
