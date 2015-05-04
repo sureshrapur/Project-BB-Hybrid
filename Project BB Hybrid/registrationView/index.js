0f880963b9ed4f06c89753da6bbb9c13ff58a75d
Suresh Rapur <suresh@sjsoftech.com>
2015-05-04 10:12:01 +0000

    Changes from Screen Builder

--- Project BB Hybrid/registrationView/index.js
+++ Project BB Hybrid/registrationView/index.js
@@ -3,3 +3,14 @@
 app.registrationView = kendo.observable({
     onShow: function() {}
 });
+(function(parent) {
+    var registrationViewModel = kendo.observable({
+        fields: {
+            tel: '',
+            dropdownlist: '',
+        },
+        submit: function() {}
+    });
+
+    parent.set('registrationViewModel', registrationViewModel);
+})(app.registrationView);
