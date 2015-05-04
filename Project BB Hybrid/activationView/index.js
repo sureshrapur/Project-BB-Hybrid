f052a318fb35eee8d36e9912097af463de2f808b
Suresh Rapur <suresh@sjsoftech.com>
2015-05-04 09:48:30 +0000

    Changes from Screen Builder

---
+++ Project BB Hybrid/activationView/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.activationView = kendo.observable({
+    onShow: function() {}
+});
---
+++ Project BB Hybrid/profileView/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+app.profileView = kendo.observable({
+    onShow: function() {}
+});
